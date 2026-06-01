efa1af14ba8face93998c7b49913eb95b7fd2804
TristanMrt <tristanmiramont14@gmail.com>
2026-06-01 10:03:28 +0200

    protection admin

---
+++ admin/load.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$allowed = ['actus', 'palmares', 'tournois', 'tarifs', 'horaires', 'histoire', 'resultats'];
+
+$fichier = $_GET['fichier'] ?? '';
+
+if (!in_array($fichier, $allowed)) {
+    echo json_encode([]);
+    exit;
+}
+
+$chemin = '../data/' . $fichier . '.json';
+
+if (file_exists($chemin)) {
+    echo file_get_contents($chemin);
+} else {
+    echo json_encode([]);
+}
+?>
