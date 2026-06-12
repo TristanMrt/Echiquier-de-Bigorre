7f1050a9decbaf328e7b4ee315564987dbf8954a
TristanMrt <tristanmiramont14@gmail.com>
2026-06-12 10:52:01 +0200

    la fin

--- admin/load.php
+++ admin/load.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-$allowed = ['actus', 'palmares', 'tournois', 'tarifs', 'horaires', 'histoire', 'resultats', 'liens_tarifs', 'activites'];
+$allowed = ['actus', 'palmares', 'tournois', 'tarifs', 'horaires', 'histoire', 'resultats', 'liens_tarifs', 'activites', 'messages'];
 
 $fichier = $_GET['fichier'] ?? '';
 
